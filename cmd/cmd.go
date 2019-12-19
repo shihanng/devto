e59fc7ee373cb8010a81791c518f2d475a1d057c
Shi Han NG <shihanng@gmail.com>
2019-12-19 21:55:46 +0900

    cmd: Add list subcommand

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -3,8 +3,6 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"log"
-	"os"
 
 	"github.com/shihanng/devto/pkg/devto"
 	"github.com/spf13/cobra"
@@ -14,22 +12,39 @@ func New() *cobra.Command {
 	rootCmd := &cobra.Command{
 		Use:   "devto",
 		Short: "Publish to dev.to from your terminal",
-		Run: func(cmd *cobra.Command, args []string) {
-
-			auth := context.WithValue(context.Background(), devto.ContextAPIKey, devto.APIKey{
-				Key: os.Getenv("DEV_TOKEN"),
-			})
-
-			client := devto.NewAPIClient(devto.NewConfiguration())
-			articles, _, err := client.ArticlesApi.GetUserAllArticles(auth, nil)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			for _, a := range articles {
-				fmt.Println(a.Title, a.Id)
-			}
-		},
 	}
+	rootCmd.PersistentFlags().String("api-key", "", "API key for authentication")
+
+	listCmd := &cobra.Command{
+		Use:   "list",
+		Short: "List published articles on dev.to",
+		RunE:  listRunE,
+	}
+
+	rootCmd.AddCommand(listCmd)
+
 	return rootCmd
+}
+
+func listRunE(cmd *cobra.Command, args []string) error {
+	apiKey, err := cmd.Parent().PersistentFlags().GetString("api-key")
+	if err != nil {
+		return err
+	}
+
+	auth := context.WithValue(context.Background(), devto.ContextAPIKey, devto.APIKey{
+		Key: apiKey,
+	})
+
+	client := devto.NewAPIClient(devto.NewConfiguration())
+	articles, _, err := client.ArticlesApi.GetUserAllArticles(auth, nil)
+	if err != nil {
+		return err
+	}
+
+	for _, a := range articles {
+		fmt.Println(a.Title, a.Id)
+	}
+
+	return nil
 }
