4b24a61c4c9f31e6ef1b200364d59cf41077f7ec
Shi Han NG <shihanng@gmail.com>
2019-12-20 22:22:39 +0900

    cmd: Add config and flag for api-key

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -3,17 +3,24 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"strings"
 
+	"github.com/cockroachdb/errors"
 	"github.com/shihanng/devto/pkg/devto"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const (
+	flagAPIKey = "api-key"
 )
 
 func New() *cobra.Command {
-	rootCmd := &cobra.Command{
-		Use:   "devto",
-		Short: "Publish to dev.to from your terminal",
-	}
-	rootCmd.PersistentFlags().String("api-key", "", "API key for authentication")
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	viper.SetEnvPrefix("DEVTO")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 
 	listCmd := &cobra.Command{
 		Use:   "list",
@@ -21,23 +28,45 @@ func New() *cobra.Command {
 		RunE:  listRunE,
 	}
 
+	rootCmd := &cobra.Command{
+		Use:               "devto",
+		Short:             "Publish to dev.to from your terminal",
+		PersistentPreRunE: rootRunE,
+	}
+
+	rootCmd.PersistentFlags().String(flagAPIKey, "", "API key for authentication")
 	rootCmd.AddCommand(listCmd)
+
+	viper.BindPFlag(flagAPIKey, rootCmd.PersistentFlags().Lookup(flagAPIKey))
 
 	return rootCmd
 }
 
-func listRunE(cmd *cobra.Command, args []string) error {
-	apiKey, err := cmd.Parent().PersistentFlags().GetString("api-key")
-	if err != nil {
-		return err
+func rootRunE(cmd *cobra.Command, args []string) error {
+	if err := viper.ReadInConfig(); err != nil {
+		if !errors.As(err, &viper.ConfigFileNotFoundError{}) {
+			return errors.Wrap(err, "cmd: read config")
+		}
 	}
 
-	auth := context.WithValue(context.Background(), devto.ContextAPIKey, devto.APIKey{
-		Key: apiKey,
+	config := struct {
+		APIKey string `mapstructure:"api-key"`
+	}{}
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return errors.Wrap(err, "cmd: unmarshal config")
+	}
+
+	return nil
+}
+
+func listRunE(cmd *cobra.Command, args []string) error {
+	apiKey := context.WithValue(context.Background(), devto.ContextAPIKey, devto.APIKey{
+		Key: viper.GetString(flagAPIKey),
 	})
 
 	client := devto.NewAPIClient(devto.NewConfiguration())
-	articles, _, err := client.ArticlesApi.GetUserAllArticles(auth, nil)
+	articles, _, err := client.ArticlesApi.GetUserAllArticles(apiKey, nil)
 	if err != nil {
 		return err
 	}
--- go.mod
+++ go.mod
@@ -4,6 +4,14 @@ go 1.13
 
 require (
 	github.com/antihax/optional v1.0.0
+	github.com/certifi/gocertifi v0.0.0-20191021191039-0944d244cd40 // indirect
+	github.com/cockroachdb/errors v1.2.4
+	github.com/cockroachdb/logtags v0.0.0-20190617123548-eb05cc24525f // indirect
+	github.com/getsentry/raven-go v0.2.0 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.3.2
 	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6
 )
